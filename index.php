468474df35479cd1b7dec011ee1553b32fe9bf9a
Katsuro Kurosaki <kurosaki@katsunet.com>
2018-02-21 18:00:14 +0100

    Added remoteModal.php and changed a little index.php

--- index.php
+++ index.php
@@ -10,7 +10,19 @@
 		<main role="main" class="container">
 			<div class="starter-template">
 				<h1>Bootstrap starter template</h1>
-				<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+				<hr/>
+				<p class="lead">Animatecss</p>
+				<pre>
+$("body").animateCss({
+	name:"zoomOut",
+	duration:"3s",
+	end:function(){
+		alert("Finish!");
+	}
+});
+				</pre>
+				<hr/>
+				
 			</div>
 		</main>
 
---
+++ remoteModal.php
@@ -0,0 +1,10 @@
+<div class="modal-header">
+	<h4 class="modal-title">Title</h4>
+	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+</div>
+<div class="modal-body">
+	Body
+</div>
+<div class="modal-footer">
+	<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+</div>
