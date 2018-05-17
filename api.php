70058445e8eedec2bd72a2e328cf6dbda4afad10
Katsuro Kurosaki <joaquimsb89@gmail.com>
2018-05-17 13:09:23 +0200

    5 decimal number instead of 6 to prevent scientific notation.

--- api.php
+++ api.php
@@ -62,7 +62,7 @@ if (\GlobalConf::API_DEBUG){
 	$_out['mem']['system_curr'] = memory_get_usage(true);
 	$_out['mem']['engine_peak'] = memory_get_peak_usage(false);
 	$_out['mem']['system_peak'] = memory_get_peak_usage(true);
-	$_out['time'] = round(microtime(TRUE)-$start_time,6);
+	$_out['time'] = round(microtime(TRUE)-$start_time,5);
 }
 echo json_encode($_out);
 ?>
