a9eecea98b37a576fa7fa4e10a2b4726ce3e727d
Katsuro Kurosaki <joaquimsb89@gmail.com>
2018-08-03 17:04:19 +0200

    They need their personal space.

--- js/animatecss.js
+++ js/animatecss.js
@@ -3,12 +3,12 @@
 (function($){
 	$.fn.animateCss = function (options) {
 		var _settings = $.extend({
-			effect: "bounce",	// One of the effects on the list above.
-			duration: "1s",		// Amount of seconds that the effect will be active.
-			delay: "0s",		// Amount of seconds before the effect begins.
-			infinite:false,		// Will the effect run infinitely?
-			begin:function(){},	// Function to execute before the effect starts. Doesn't care about delay.
-			end:function(){}	// Function to execute when the effect ends. Won't run if infinite:true!
+			effect: "bounce",		// One of the effects on the list above.
+			duration: "1s",			// Amount of seconds that the effect will be active.
+			delay: "0s",			// Amount of seconds before the effect begins.
+			infinite: false,		// Will the effect run infinitely?
+			begin: function(){},	// Function to execute before the effect starts. Doesn't care about delay.
+			end: function(){}		// Function to execute when the effect ends. Won't run if infinite:true!
 		},options);
 		
 		_settings.begin();
