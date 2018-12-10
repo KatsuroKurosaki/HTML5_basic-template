99bd753d808b899c5994001f813842ec4c528e87
Katsuro Kurosaki <joaquimsb89@gmail.com>
2018-12-10 11:52:42 +0100

    A better implementation of footerjscss.php imho

--- footerjscss.php
+++ footerjscss.php
@@ -1,21 +1,21 @@
 <!-- jQuery -->
-<script type="text/javascript" src="./js/jquery.min-3.3.1.js" charset="UTF-8"></script>
+<script src="js/jquery.min-3.3.1.js?_=<?=filemtime('js/jquery.min-3.3.1.js');?>" type="text/javascript" charset="UTF-8"></script>
 
 <!-- Bootstrap -->
-<script type="text/javascript" src="./js/bootstrap.bundle.min-4.1.3.js" charset="UTF-8"></script>
-<link rel="stylesheet" type="text/css" href="./css/bootstrap.min-4.1.3.css"/>
-<link rel="stylesheet" type="text/css" href="./css/bootswatch/cerulean/bootstrap.min.css"/>
+<script src="js/bootstrap.bundle.min-4.1.3.js?_=<?=filemtime('js/bootstrap.bundle.min-4.1.3.js');?>" type="text/javascript" charset="UTF-8"></script>
+<link href="css/bootstrap.min-4.1.3.css?_=<?=filemtime('css/bootstrap.min-4.1.3.css');?>" type="text/css" rel="stylesheet"/>
+<link href="css/bootswatch/cerulean/bootstrap.min.css?_=<?=filemtime('css/bootswatch/cerulean/bootstrap.min.css');?>" type="text/css" rel="stylesheet"/>
 
 <!-- Animate.css -->
-<script type="text/javascript" src="./js/animatecss.js" charset="UTF-8"></script>
-<link rel="stylesheet" type="text/css" href="./css/animate.min-3.7.0.css"/>
+<script src="js/animatecss.js?_=<?=filemtime('js/animatecss.js');?>" type="text/javascript" charset="UTF-8"></script>
+<link href="css/animate.min-3.7.0.css?_=<?=filemtime('css/animate.min-3.7.0.css');?>" type="text/css" rel="stylesheet"/>
 
 <!-- Font Awesome -->
-<link rel="stylesheet" type="text/css" href="./css/fontawesome-all.min-5.1.1.css"/>
+<link href="css/fontawesome-all.min-5.1.1.css?_=<?=filemtime('css/fontawesome-all.min-5.1.1.css');?>" type="text/css" rel="stylesheet"/>
 
 <!-- filesizejs -->
-<script type="text/javascript" src="./js/filesize.min-3.6.1.js" charset="UTF-8"></script>
+<script src="js/filesize.min-3.6.1.js?_=<?=filemtime('js/filesize.min-3.6.1.js');?>" type="text/javascript" charset="UTF-8"></script>
 
 <!-- Global resources -->
-<script type="text/javascript" src="./js/common_func.js" charset="UTF-8"></script>
-<link rel="stylesheet" type="text/css" href="./css/common_style.css"/>
+<script src="js/common_func.js?_=<?=filemtime('js/common_func.js');?>" type="text/javascript" charset="UTF-8"></script>
+<link href="css/common_style.css?_=<?=filemtime('css/common_style.css');?>" type="text/css" rel="stylesheet"/>
