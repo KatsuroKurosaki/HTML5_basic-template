ec39cb586f919b0dacb74a6cbef09ca84cfe40ff
Joaquim Sardiña <joaquimsb89@gmail.com>
2019-01-28 10:49:28 +0100

    Integrated animatecss.js into common_func.js

--- footerjs.php
+++ footerjs.php
@@ -4,8 +4,5 @@
 <!-- Bootstrap -->
 <script src="js/bootstrap.bundle.min-4.2.1.js?_=<?=filemtime('js/bootstrap.bundle.min-4.2.1.js');?>" type="text/javascript" charset="UTF-8"></script>
 
-<!-- Animate.css -->
-<script src="js/animatecss.js?_=<?=filemtime('js/animatecss.js');?>" type="text/javascript" charset="UTF-8"></script>
-
 <!-- Global functions -->
 <script src="js/common_func.js?_=<?=filemtime('js/common_func.js');?>" type="text/javascript" charset="UTF-8"></script>
