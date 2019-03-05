82c2a05343aa1febf52df55254521fb01bc0fddd
Joaquim Sardiña <joaquimsb89@gmail.com>
2019-03-05 16:54:28 +0100

    Cleaned up login code.

--- login.php
+++ login.php
@@ -5,19 +5,29 @@
 		<?php require 'headercss.php'; ?>
 		<link href="css/login_style.css?_=<?=filemtime('css/login_style.css');?>" type="text/css" rel="stylesheet" />
 	</head>
-	<body class="d-flex justify-content-center align-items-center text-center">
-		<form class="form-signin invisible">
-			<h1 class="h3 mb-3 font-weight-normal text-light">
-				<i class="fas fa-lock"></i> Restricted area
-			</h1>
-			<label class="sr-only">Username</label>
-			<input type="text" name="username" class="form-control" placeholder="Username" />
-			<label class="sr-only">Password</label>
-			<input type="password" name="password" class="form-control" placeholder="Password" />
-			<button class="btn btn-lg btn-primary btn-block mt-3 mb-3" type="submit">
-				<i class="fas fa-sign-in-alt"></i> Log in
-			</button>
-			<p class="mb-3 text-light small">&copy; Company <?=date('Y');?></p>
+	<body class="d-flex justify-content-center align-items-center">
+		<form class="form-row form-signin text-light px-3 py-4 invisible">
+			<div class="col-12 mb-3">
+				<h3 class="text-center">
+					<i class="fas fa-lock"></i> Restricted area
+				</h3>
+			</div>
+			<div class="col-12 mb-3">
+				<label>Username:</label>
+				<input type="text" name="username" class="form-control" />
+			</div>
+			<div class="col-12 mb-3">
+				<label>Password:</label>
+				<input type="password" name="password" class="form-control" />
+			</div>
+			<div class="col-12 mb-3">
+				<button type="submit" class="btn btn-lg btn-primary btn-block">
+					<i class="fas fa-sign-in-alt"></i> Log in
+				</button>
+			</div>
+			<div class="col-12">
+				<p class="small text-center">&copy; <?=date('Y');?> - Company</p>
+			</div>
 		</form>
 		<?php require 'footerjs.php'; ?>
 		<script src="js/functions.js?_=<?=filemtime('js/functions.js');?>" type="text/javascript" charset="UTF-8"></script>
