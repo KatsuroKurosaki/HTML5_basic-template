3613f737ac5745552f061fb4211f49c46ec51203
Joaquim Sardiña <joaquimsb89@gmail.com>
2019-09-17 13:41:56 +0200

    Added second parameter to toObject() method: It hides null value properties

---
+++ api/Core/BaseClass.php
@@ -0,0 +1,51 @@
+<?php
+namespace Core;
+
+class BaseClass extends \Core\BaseObject
+{
+
+    protected $id = null;
+    protected $key = null;
+
+    /** @var string */
+    protected $language = "esp";
+    /** @var string */
+    protected $defaultLanguage = "esp";
+
+    /**
+     * BaseClass constructor.
+     * @param string $language
+     */
+    public function __construct(string $language = "esp")
+    {
+        $this->__set("language", $language);
+    }
+
+    /** Getters **/
+    public function getId()
+    {
+        return $this->__get("id");
+    }
+
+    public function getLanguage()
+    {
+        return $this->__get("language");
+    }
+
+    public function getDefaultLanguage()
+    {
+        return $this->__get("defaultLanguage");
+    }
+
+    /** Setters **/
+    public function setId(int $value)
+    {
+        $this->__set("id", $value);
+    }
+
+    public function setKey(int $value)
+    {
+        $this->__set("key", $value);
+    }
+
+}
