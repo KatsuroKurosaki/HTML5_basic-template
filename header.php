0e9979e73e113efc1e7c3379967753e73bbbf443
Joaquim Sardiña <joaquimsb89@gmail.com>
2019-11-11 16:20:58 +0100

    Reorganized header and changed icon

--- header.php
+++ header.php
@@ -2,13 +2,15 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 <meta name="format-detection" content="telephone=no" />
-<meta name="msapplication-tap-highlight" content="no" />
 <meta name="msapplication-config" content="none" />
-<meta name="description" content="" />
 <meta name="author" content="Katsuro Kurosaki" />
+<meta name="generator" content="PhosphorPHP" />
+<meta name="description" content="Basic PhosphorPHP project to start from scratch." />
 <meta name="theme-color" content="#000000" />
 
-<link href="favicon.png" rel="icon" type="image/png" sizes="32x32" />
-<link href="manifest.json" rel="manifest" />
+<link rel="icon" sizes="192x192" href="favicon.png">
+<link rel="apple-touch-icon" href="favicon.png">
 
-<title>Basic Bootstrap template</title>
+<link rel="manifest" href="manifest.json" />
+
+<title>PhosphorPHP UI</title>
