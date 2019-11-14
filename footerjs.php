12a307a70d10e18060057c96cabf4e89d6562c05
Joaquim Sardiña <joaquimsb89@gmail.com>
2019-11-14 12:23:39 +0100

    Renamed requires

--- footerjs.php
+++
@@ -1,8 +0,0 @@
-<!-- jQuery -->
-<script src="js/jquery.min-3.4.1.js" type="text/javascript" charset="UTF-8"></script>
-
-<!-- Bootstrap -->
-<script src="js/bootstrap.bundle.min-4.3.1.js" type="text/javascript" charset="UTF-8"></script>
-
-<!-- Global functions -->
-<script src="js/common_func.min.js?<?=filemtime('js/common_func.min.js')?>" type="text/javascript" charset="UTF-8"></script>
--- headercss.php
+++
@@ -1,9 +0,0 @@
-<!-- Bootstrap -->
-<link href="css/bootstrap.min-4.3.1.css" type="text/css" rel="stylesheet" />
-<!--<link href="css/bootswatch/cerulean/bootstrap.min.css?<?=filemtime('css/bootswatch/cerulean/bootstrap.min.css')?>" type="text/css" rel="stylesheet" />-->
-
-<!-- Animate.css -->
-<link href="css/animate.min-3.7.2.css" type="text/css" rel="stylesheet" />
-
-<!-- Global styles -->
-<link href="css/common_style.min.css?<?=filemtime('css/common_style.min.css')?>" type="text/css" rel="stylesheet" />
