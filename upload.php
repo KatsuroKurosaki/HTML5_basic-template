c950a55e294cfef13b2a2bfe21f20ea6a9bb9511
Joaquim Sardiña <joaquimsb89@gmail.com>
2020-02-10 17:15:09 +0100

    Prepare for multiple file upload

--- upload.php
+++ upload.php
@@ -7,25 +7,32 @@
 </head>
 
 <body>
-	<form id="formUpload" enctype="multipart/form-data" action="receive.php" method="post" class="container mt-2">
-		<div class="custom-file">
-			<input type="file" class="custom-file-input" name="file" onchange="javascript:fileChange(event);" /> <!-- Filter file type: accept="image/*" -->
-			<label class="custom-file-label">Choose file...</label>
-		</div>
-		<div class="my-2">
-			<pre id="divProgress">Waiting...</pre>
-		</div>
-		<div class="mb-2">
-			<progress class="progress w-100"></progress>
-		</div>
-		<div class="text-center">
-			<button type="button" class="btn btn-lg btn-success mb-2" onclick="javascript:uploadAjax();">Upload</button>
-		</div>
-		<div class="text-center">
-			<img src="" class="img-thumbnail" style="display:none;" />
-		</div>
-		<input type="hidden" name="op" value="UPLOAD_FILE" />
-	</form>
+	<?php define("_FILE", basename(__FILE__, '.php'));
+	require 'navbar.php'; ?>
+
+	<main role="main" class="container pt-2 pb-5">
+		<form id="formUpload" enctype="multipart/form-data" action="receive.php" method="post">
+			<div class="custom-file">
+				<input type="file" class="custom-file-input" name="file" onchange="javascript:fileChange(event);" /> <!-- Filter file type: accept="image/*" -->
+				<label class="custom-file-label">Choose file...</label>
+			</div>
+			<div class="my-2">
+				<pre id="divProgress">Waiting...</pre>
+			</div>
+			<div class="mb-2">
+				<progress class="progress w-100"></progress>
+			</div>
+			<div class="text-center">
+				<button type="button" class="btn btn-lg btn-success mb-2" onclick="javascript:uploadAjax();">Upload</button>
+			</div>
+			<div class="text-center">
+				<img src="" class="img-thumbnail" style="display:none;" />
+			</div>
+			<input type="hidden" name="op" value="UPLOAD_FILE" />
+		</form>
+		<hr />
+		<label>Coming soon: Multiple file upload</label>
+	</main>
 	<?php require 'footer.php'; ?>
 	<?php require 'footerjs.htm'; ?>
 	<script src="js/upload.min.js?<?= filemtime('js/upload.min.js') ?>"></script>
