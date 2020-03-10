5d93d638ebf20276b31510706edaaa328d7cf767
Joaquim Sardiña <joaquimsb89@gmail.com>
2020-03-10 08:40:17 +0100

    New html headers

--- header.php
+++ header.php
@@ -1,13 +1,16 @@
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
 <meta name="format-detection" content="telephone=no" />
 <meta name="msapplication-config" content="none" />
+<meta name="msapplication-tap-highlight" content="no" />
 <meta name="google" content="notranslate" />
 <meta name="author" content="Katsuro Kurosaki" />
 <meta name="generator" content="PhosphorPHP" />
 <meta name="description" content="Basic PhosphorPHP project to start from scratch." />
 <meta name="theme-color" content="#800080" />
+
+<!-- <meta http-equiv="Content-Security-Policy" content="default-src 'self'"> -->
 
 <link rel="icon" sizes="192x192" href="favicon.png">
 <link rel="apple-touch-icon" href="favicon.png">
