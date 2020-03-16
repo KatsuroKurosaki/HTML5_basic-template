2ebaa6e98e1ba90538fde17b5d0bf862244c17e5
Joaquim Sardiña <joaquimsb89@gmail.com>
2020-03-16 17:05:19 +0100

    Added $.hexEncode and $.hexDecode

--- libs/phosphorphp-ui/phosphorphp-ui-1.0.js
+++ libs/phosphorphp-ui/phosphorphp-ui-1.0.js
@@ -91,6 +91,29 @@
 			return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/ /g, '&nbsp;');
 		},
 
+		hexEncode: function () {
+			var hex, i;
+
+			var result = "";
+			for (i = 0; i < this.length; i++) {
+				hex = this.charCodeAt(i).toString(16);
+				result += ("000" + hex).slice(-4);
+			}
+
+			return result
+		},
+
+		hexDecode = function () {
+			var j;
+			var hexes = this.match(/.{1,4}/g) || [];
+			var back = "";
+			for (j = 0; j < hexes.length; j++) {
+				back += String.fromCharCode(parseInt(hexes[j], 16));
+			}
+
+			return back;
+		},
+
 		calculateAspectRatio: function (srcWidth, srcHeight, maxWidth, maxHeight) {
 			var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
 			return {
