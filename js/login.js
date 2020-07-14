ae2aa76021da9937d4191ca797c3b0c4d9cdb418
Joaquim Sardiña <joaquimsb89@gmail.com>
2020-07-14 13:31:44 +0200

    Implemented new login style

--- js/login.js
+++ js/login.js
@@ -1,20 +1,12 @@
 $(window).on("load", function () {
-	$("form").css("display", "").on("submit", function (e) {
-		e.preventDefault();
-		$.api({
-			data: {
-				op: 'HELLO'
-			},
-			success: function (data) {
-				$.spawnAlert({
-					body: data.msg
-				});
-			}
-		});
-	}).animateCss({
-		effect: "zoomIn",
-		end: function () {
-			$("form input:first").focus();
+	$("form input:first").focus();
+});
+
+function submitLogin() {
+	$.api({
+		data: $("form.form-signin").serializeForm(),
+		success: function (data) {
+
 		}
 	});
-});
+}
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
 	<?php require 'header.php'; ?>
@@ -7,30 +7,41 @@
 	<link rel="stylesheet" href="css/login_style.min.css?<?= filemtime('css/login_style.min.css') ?>" />
 </head>
 
-<body class="d-flex justify-content-center align-items-center">
-	<form class="form-row form-signin text-light px-3 py-4" style="display:none;">
-		<div class="col-12 mb-3">
-			<h3 class="text-center">
-				<i class="fas fa-lock"></i> Restricted area
-			</h3>
+<body class="h-100 d-flex justify-content-center align-items-center">
+	<div class="row no-gutters signin bgcolor-white rounded">
+		<div class="col-12 col-md-5">
+			<div class="d-flex flex-column h-100">
+				<div class="flex-fill w-75 mx-auto p-3 d-flex justify-content-center align-items-center">
+					<form class="form-row form-signin">
+						<div class="col-12 mb-3">
+							<img src="img/logo-login.png" class="d-block d-md-none mx-auto w-25" />
+							<p class="h3 font-weight-bolder text-center txtcolor-gray">BIENVENIDO</p>
+						</div>
+						<div class="col-12 mt-3 mb-3 position-relative usericon">
+							<input type="text" name="username" class="form-control form-control pl-5" placeholder="Usuario" maxlength="64" autofocus>
+						</div>
+						<div class="col-12 mb-3 position-relative passicon">
+							<input type="password" name="password" class="form-control form-control pl-5" placeholder="Contraseña" maxlength="72">
+						</div>
+						<div class="col-12 mb-3 text-center">
+							<button type="button" class="btn btn-info w-50 shadow" onclick="javascript:submitLogin();">
+								ENTRAR
+							</button>
+						</div>
+						<input type="hidden" name="op" value="LOGIN" />
+					</form>
+				</div>
+				<div class="text-center p-3">
+					<img src="img/stw-logo.png" class="img-fluid w-50" />
+				</div>
+			</div>
 		</div>
-		<div class="col-12 mb-3">
-			<label>Username:</label>
-			<input type="text" name="username" class="form-control" />
+		<div class="col-7 d-none d-md-block">
+			<div class="d-flex justify-content-center align-items-center h-100 rounded-right sidelogo">
+				<img src="img/logo-login.png" class="w-50" />
+			</div>
 		</div>
-		<div class="col-12 mb-3">
-			<label>Password:</label>
-			<input type="password" name="password" class="form-control" />
-		</div>
-		<div class="col-12 mb-3">
-			<button type="submit" class="btn btn-lg btn-primary btn-block">
-				<i class="fas fa-sign-in-alt"></i> Log in
-			</button>
-		</div>
-		<div class="col-12">
-			<p class="small text-center">&copy; <?= date('Y') ?> - Company</p>
-		</div>
-	</form>
+	</div>
 	<?php require 'footerjs.htm'; ?>
 	<script src="js/login.min.js?<?= filemtime('js/login.min.js') ?>"></script>
 </body>
