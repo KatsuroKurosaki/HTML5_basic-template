107e634e9251064cf0faf80250cbfe9a7a1c24c0
Joaquim Sardiña <joaquimsb89@gmail.com>
2021-01-22 12:45:05 +0100

    Added setCookie fuction

--- class/Utils/Functions.php
+++ class/Utils/Functions.php
@@ -4,7 +4,7 @@ namespace Utils;
 
 class Functions
 {
-	public static function returnOut($options = [])
+	public static function returnOut(array $options = [])
 	{
 		$_settings = array_replace_recursive([
 			'status' => 'ok',
@@ -19,5 +19,32 @@ class Functions
 	public static function trimSeveral(string $str)
 	{
 		return trim(preg_replace('/(?:\s\s+|\n|\t)/', ' ', $str));
+	}
+
+	public static function setCookie(array $options = [])
+	{
+		$_settings = array_replace_recursive([
+			'name' => 'cookie',
+			'value' => 'cookie',
+			'expires' => time() + 60 * 60 * 24 * 30, // 30 days,
+			'path' => '/',
+			'domain' => '',
+			'secure' => true,
+			'httponly' => true,
+			'samesite' => 'Strict'
+		], $options);
+
+		setcookie(
+			$_settings['name'],
+			$_settings['value'],
+			array(
+				'expires' => $_settings['expires'],
+				'path' => $_settings['path'],
+				'domain' => $_settings['domain'],
+				'secure' => $_settings['secure'],
+				'httponly' => $_settings['httponly'],
+				'samesite' => $_settings['samesite'],
+			)
+		);
 	}
 }
